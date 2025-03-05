f1750700d8dcbcc1a2d0deff48cc3f67456ec8d0
alyssonCode <alysson91santos@gmail.com>
2025-03-05 15:49:17 -0300

    Adding class generated from StarUML

---
+++ code/level.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.window = None
+        self.name = None
+        self.entity_list = None
+        self.Attribute1 = None
+
+    def Operation1(self, ):
+        pass
+
+    def run(self, ):
+        pass
