d3677ca4d423b3d54f06a27ec939fe80aa539830
alyssonCode <alysson91santos@gmail.com>
2025-03-10 22:05:05 -0300

    Inserção da nave do Player 1

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -4,7 +4,8 @@
 import random
 
 from code.Background import Background
-from code.Const import WIN_WIDTH
+from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Player import Player
 
 
 class EntityFactory:
@@ -18,3 +19,5 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
+            case 'Player1':
+                return Player('Player1', (10, WIN_HEIGHT / 2))
