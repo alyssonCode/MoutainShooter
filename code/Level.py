361db972f893b4f6c33c470860c549c4ac751bde
alyssonCode <alysson91santos@gmail.com>
2025-03-10 21:41:27 -0300

    Inserção de música para o nível 1 Inserção de texto para a tela do nível 1

--- code/Level.py
+++ code/Level.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+from tkinter.font import Font
+
 import pygame.display
+from pygame.rect import Rect
+from pygame.surface import Surface
+
+from code.Const import C_WHITE, WIN_HEIGHT
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
 
@@ -12,20 +19,32 @@ class Level:
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
-        # player = EntityFactory.get_entity('Player1')
-        # player.score = player_score[0]
-        # self.entity_list.append(player)
-        # if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:
-        #     player = EntityFactory.get_entity('Player2')
-        #     player.score = player_score[1]
-        #     self.entity_list.append(player)
-        # pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
-        # pygame.time.set_timer(EVENT_TIMEOUT, TIMEOUT_STEP)  # 100ms
+        self.timeout = 20000  # 20 segundos
 
     def run(self):
+        pygame.mixer_music.load(f'./asset/{self.name}.mp3')
+        pygame.mixer_music.play(-1)  # -1, toca a música pra sempre
+        clock = pygame.time.Clock()  # definição de clock para não variar de cordo com a máquina
         while True:
+            clock.tick(60) # 60 fps
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            # printed text
+            self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000:.1f}s', C_WHITE, (10, 5))
+            self.level_text(14, f'fps: {clock.get_fps():.0f}', C_WHITE, (10, WIN_HEIGHT - 35))
+            self.level_text(14, f'entidades: {len(self.entity_list)}', C_WHITE, (10, WIN_HEIGHT - 20))
             pygame.display.flip()
         pass
+
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
