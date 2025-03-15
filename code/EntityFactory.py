1c841c963378f67635ee17b8c20658bbea8523f0
alyssonCode <alysson91santos@gmail.com>
2025-03-15 15:13:02 -0300

    Creating Players and Enemies Moving Players and Enemies

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 import random
 
 from code.Background import Background
 from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Enemy import Enemy
 from code.Player import Player
 
 
@@ -20,4 +20,13 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
             case 'Player1':
-                return Player('Player1', (10, WIN_HEIGHT / 2))
+                return Player('Player1', (10, WIN_HEIGHT / 2 -30))
+
+            case 'Player2':
+                return Player('Player2', (10, WIN_HEIGHT / 2 +30))
+
+
+            case 'Enemy1':
+                return Enemy('Enemy1', (WIN_WIDTH +10, random.randint(40, WIN_HEIGHT - 40))) # gera os inimigos antes da tela e gera alturas aleatórias dentro do intervalo
+            case 'Enemy2':
+                return Enemy('Enemy2', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
--- code/Level.py
+++ code/Level.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import random
 import sys
 from tkinter.font import Font
 
@@ -7,20 +8,28 @@ import pygame.display
 from pygame.rect import Rect
 from pygame.surface import Surface
 
-from code.Const import C_WHITE, WIN_HEIGHT
+from code.Const import C_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME, EVENT_TIMEOUT, TIMEOUT_STEP
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
 
 
 class Level:
     def __init__(self, window, name, game_mode):
+
+        self.timeout = 20000  # 20 segundos
         self.window = window
         self.name = name
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
         self.entity_list.append(EntityFactory.get_entity('Player1'))
-        self.timeout = 20000  # 20 segundos
+
+        if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:
+            self.entity_list.append(EntityFactory.get_entity('Player2'))
+
+        # Cria um inimigo a cada X tempo
+        pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
+
 
     def run(self):
         pygame.mixer_music.load(f'./asset/{self.name}.mp3')
@@ -35,6 +44,9 @@ class Level:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == EVENT_ENEMY:
+                    choice = random.choice(('Enemy1', 'Enemy2'))
+                    self.entity_list.append(EntityFactory.get_entity(choice))
 
             # printed text
             self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000:.1f}s', C_WHITE, (10, 5))
